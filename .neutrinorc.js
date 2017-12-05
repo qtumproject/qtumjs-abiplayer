30cec441d7abd05950becc7846bfe0aa5db43b57
Howard Yeh <howard@metacircus.com>
2017-12-05 11:01:37 +0800

    disable ordered import lint

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -1,8 +1,28 @@
+const webpack = require("webpack")
+const appConfig = require("config")
 const reactTSPreset = require("neutrino-preset-react-typescript")
 
 const customSourcemap = ({ config }) => {
   config.when(!!process.env.SOURCEMAP, config => config.devtool(process.env.SOURCEMAP))
 }
+
+const defineGlobals = ({ config }) => {
+  const constants = appConfig.constants
+
+  const globals = {}
+
+  // JSON stringify all constants defined in config.constants.
+  // Since webpack.DefinePlugin splice these values as source code,
+  // we are effectively treating them as JS literals.
+  Object.keys(constants).map((key) => {
+    const val = constants[key]
+    globals[key] = JSON.stringify(val, null, "  ")
+  })
+
+  config.plugin("globals")
+    .use(webpack.DefinePlugin, [globals])
+}
+
 
 module.exports = (neutrino, opts = {}) => {
   neutrino.use(reactTSPreset, {
@@ -16,4 +36,6 @@ module.exports = (neutrino, opts = {}) => {
   })
 
   neutrino.use(customSourcemap)
+
+  neutrino.use(defineGlobals)
 }
